a491249303c7c872cb90a50a6e575152a603bb05
arzafran <arzafran@gmail.com>
2017-05-03 22:00:35 -0300

    Added previous price saving and  some glitter

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 export default class App extends React.Component {
   constructor(){
     super();
-    this.state = { pricePerGram: '--' };
+    this.state = { 
+      previousPrice: 0,
+      pricePerGram: 0 
+    };
     this.update = this.update.bind(this);
+    this.save = this.save.bind(this);
   }
+
   update(){
     let price = this.refs.price.value;
     let grams = this.refs.grams.value;
@@ -15,9 +20,15 @@ export default class App extends React.Component {
       const result = price / grams;
       this.setState({pricePerGram: result.toFixed(3)})
     } else {
-      this.setState({pricePerGram: '--'})
+      this.setState({pricePerGram: 0})
     }
   }
+
+  save() {
+    if(this.state.pricePerGram)
+      this.setState({previousPrice: this.state.pricePerGram});
+  }
+
   render() {
     return (
       <div className="main-container">
@@ -38,7 +49,15 @@ export default class App extends React.Component {
             placeholder="Grams in package"
           />
           <label className="magicbox__label">
-          $ {this.state.pricePerGram} /Gr.
+            $ {this.state.pricePerGram} /Gr.
+          </label>
+          <button
+            className="btn"
+            onClick={this.save}>
+            SAVE
+          </button>
+          <label className="magicbox__label magicbox__label--previous">
+            $ {this.state.previousPrice} /Gr.
           </label>
         </section>
       </div>
