2bd735cc175a71b2066632de79590ed54fe0b88a
arzafran <arzafran@gmail.com>
2017-06-13 22:20:33 -0300

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const container = document.createElement('div');
-  ReactDOM.render(<App />, container);
-});
